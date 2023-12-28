a21dbade833b4911453bf3400aa6033bd5b1231e
Alberto Julián <ajulianes@yahoo.es>
2023-12-28 16:55:25 +0100

    Reads MIDI file with leadsheet (melody + chord symbols) as a music21 melody and chord symbols

---
+++ omnibook_read.py
@@ -0,0 +1,270 @@
+import os
+
+import music21 as m21
+from m21_musescore import M21_and_show
+from alex2alberto import chord_type_map_dict
+# from utils_xml_omnibook import xml_parser # from Ken Deguernel, creator of Omnibook xml
+
+CHORD_SPLIT = ":"
+admitted_durations = [1/6, 0.25, 1/3, 0.5, 0.75, 2/3, 1, 1.5, 2, 3, 4]
+non_admitted_durations = [1/5, 0.125, 4/5, 1/12]
+
+# TODO: indicar estilo swing en partitura.
+#   - A mano se puede poner en MuseScore: Formato/Estilo/Partitura - Ajustes de swing
+#   - De momento cambio duración de corcheas: 2/3, 1/3 en tiempos medios, 3/5, 2/5 en tiempos rápidos
+# HECHO: recuperar notas ligadas
+# HECHO: ver tipos de acorde en todos los temas de Omnibook. De momento, C/G => C:M7
+# HECHO: comprobar si todos los temas de Omnibook tienen el primer compás entero
+# HECHO: leer tempo de score de XML y pasarlo. Se asigna al primer compás en cellularautomaton_BillAI.py
+# HECHO: asignar armadura (key). Se asigna al primer compás en cellularautomaton_BillAI.py
+
+def chords_and_melody(omni_file):
+    score = m21.converter.parse(omni_file)
+
+    chord_dict = M21_and_show.chord_dict
+
+    part = score.parts[0]
+
+    chords_omni = []
+    melody = []
+    for element in part.flatten().notesAndRests:
+        if 'Note' in element.classes:
+            melody.append((element.nameWithOctave, element.duration.quarterLength))
+            # print(element.nameWithOctave, element.duration.quarterLength, element.offset, element.tie)
+        elif 'Rest' in element.classes:
+            melody.append(("R", element.duration.quarterLength))
+            # print("Rest", element.duration.quarterLength, element.offset)
+        elif 'Chord' in element.classes: # m21.harmony.ChordType
+            # print("CHORD", element.figure, element.root(), element.offset)
+            chord_type_omni = element.figure.strip(element.root().name)
+            chord_name = element.root().name + CHORD_SPLIT + chord_type_omni
+            chords_omni.append((chord_name, int(element.offset)))
+            # print("CHORD", chord_name, element.offset)
+
+    chord_types = []
+    chord_progression = []
+    for chord_pos, (chord_name_omni, chord_offset) in enumerate(chords_omni):
+        # for last chord, no duration can be assumed
+        if chord_pos == len(chords_omni) - 1:
+            duration = 4
+        else:
+            _, next_chord_offset = chords_omni[chord_pos + 1]
+            duration = next_chord_offset - chord_offset
+
+        chord_type_omni = chord_name_omni.split(CHORD_SPLIT)[1]
+        chord_type = chord_type_map_dict.get(chord_type_omni, chord_type_omni)
+        # if chord_type == chord_type_omni:
+            # chord not in mapping dictionary, but may be on base dicionary
+            # chord_type = chord_dict.get(chord_type_omni, chord_type_omni)
+            # if chord_type == chord_type_omni:
+            # TODO SILENCIO
+        chord_types.append(chord_type)
+
+        c_chord_name = "C" + CHORD_SPLIT + chord_type
+        if c_chord_name not in chord_dict.keys():
+            print(f"Unknown chord {chord_name_omni}")
+            chord_name = "C" + CHORD_SPLIT + "M7"
+        else:
+            chord_name = chord_name_omni.split(CHORD_SPLIT)[0] + CHORD_SPLIT + chord_type
+
+        chord_progression.append((chord_name, duration))
+
+    return chord_progression, melody, chord_types
+
+def chords_and_21melody(omni_file):
+    score = m21.converter.parse(omni_file)
+    key = score.analyze("key")
+
+    part = score.parts[0]
+    m0 = part.getElementsByClass(m21.stream.Measure)[0] # measure 0
+
+    tempo = None
+    for element in m0:
+        if isinstance(element, m21.tempo.MetronomeMark):
+            tempo = element
+
+    chord_dict = M21_and_show.chord_dict
+
+    chords_omni = []
+    melody = []
+
+
+    elements = part.flatten().notesAndRests
+    for element in elements:
+        if 'Note' in element.classes or 'Rest' in element.classes:
+
+            melody.append(element)
+            # print(element.nameWithOctave, element.duration.quarterLength, element.offset, element.tie)
+
+        elif 'Chord' in element.classes: # m21.harmony.ChordType
+            # print("CHORD", element.figure, element.root(), element.offset)
+            chord_type_omni = element.figure.strip(element.root().name)
+            chord_name = element.root().name + CHORD_SPLIT + chord_type_omni
+            chords_omni.append((chord_name, int(element.offset)))
+            # print("CHORD", chord_name, element.offset)
+
+    chord_types = []
+    chord_progression = []
+    for chord_pos, (chord_name_omni, chord_offset) in enumerate(chords_omni):
+        # for last chord, no duration can be assumed
+        if chord_pos == len(chords_omni) - 1:
+            duration = 4
+        else:
+            _, next_chord_offset = chords_omni[chord_pos + 1]
+            duration = next_chord_offset - chord_offset
+
+        chord_type_omni = chord_name_omni.split(CHORD_SPLIT)[1]
+        chord_type = chord_type_map_dict.get(chord_type_omni, chord_type_omni)
+        # if chord_type == chord_type_omni:
+            # chord not in mapping dictionary, but may be on base dicionary
+            # chord_type = chord_dict.get(chord_type_omni, chord_type_omni)
+            # if chord_type == chord_type_omni:
+            # TODO SILENCIO
+        chord_types.append(chord_type)
+
+        c_chord_name = "C" + CHORD_SPLIT + chord_type
+        if c_chord_name not in chord_dict.keys():
+            print(f"Unknown chord {chord_name_omni}")
+            chord_name = "C" + CHORD_SPLIT + "M7"
+        else:
+            chord_name = chord_name_omni.split(CHORD_SPLIT)[0] + CHORD_SPLIT + chord_type
+
+        chord_progression.append((chord_name, duration))
+
+    return chord_progression, melody, chord_types, key, tempo
+
+
+def chords_and_melody_all(files_path):
+
+    chord_types_all = set()
+    chords_all = []
+    melody_all = []
+    omni_files = os.listdir(files_path)
+    omni_files = [os.path.join(files_path, omni_file) for omni_file in omni_files if omni_file[-4:] == ".xml"]
+    for omni_file in omni_files:
+        chords, melody, chord_types = chords_and_melody(omni_file)
+        chords_all.append(chords)
+        melody_all.append(melody)
+        chord_types_all = chord_types_all.union(set(chord_types))
+
+    print(chord_types_all)
+
+    return chords_all, melody_all
+
+def file_melody(file_path, melody):
+    """
+    For Markov Chain training
+    :param file_path:
+    :param pitches:
+    :return:
+    """
+
+    score = m21.converter.parse(file_path)
+
+    # score.metadata = m21.metadata.Metadata()
+    score.append(m21.metadata.MetadataEntry('styleText', 'swing'))
+
+    # chord_dict = M21_and_show.chord_dict
+    key = score.analyze("key")
+    # key = m21.analysis.discrete.analyzeStream(score, 'Krumhansl')
+
+    if key.mode == "major":
+        interval = m21.interval.Interval(key.tonic, m21.pitch.Pitch("C"))
+    elif key.mode == "minor":
+        interval = m21.interval.Interval(key.tonic, m21.pitch.Pitch("A"))
+
+    print(key.tonic, key.mode, interval.semitones, file_path.strip("Omnibook/"))
+
+    part = score.parts[0]
+
+    m0 = part.getElementsByClass(m21.stream.Measure)[0] # measure 0
+
+    tempo = None
+    for pos, element in enumerate(m0):
+        if isinstance(element, m21.key.KeySignature):
+            m0[pos] = key
+            m0[pos] = m21.key.KeySignature(-interval.semitones)
+        if isinstance(element, m21.tempo.MetronomeMark):
+            tempo = element
+
+    for element in part.flatten().notesAndRests:
+        if 'Note' in element.classes:
+            c_midi = element.pitch.midi + interval.semitones
+            melody.append((c_midi, element.duration.quarterLength))
+
+            duration = element.duration.quarterLength
+
+            if min([abs(admitted_duration - round(duration, 4)) for admitted_duration in admitted_durations]) > 0.0001:
+                if min([abs(non_admitted_duration - round(duration, 4)) for non_admitted_duration in non_admitted_durations]) > 0.0001:
+                    print(duration)
+
+        elif 'Rest' in element.classes:
+            melody.append(("R", element.duration.quarterLength))
+
+    return melody
+
+def file_melody_all(files_path):
+
+    omni_files = os.listdir(files_path)
+    omni_files = [os.path.join(files_path, omni_file) for omni_file in omni_files if omni_file[-4:] == ".xml"]
+    melody_all = []
+    for omni_file in omni_files:
+        melody_all = file_melody(omni_file, melody_all)
+
+    return melody_all
+
+if __name__ == "__main__":
+
+    file = "swing.mxl"
+
+    score = m21.converter.parse(file)
+
+    score.show()
+
+    exit()
+
+    ONE_FILE = True
+
+    if ONE_FILE:
+        # original https://www.youtube.com/watch?v=02apSoxB7B4
+        omni_file = "Omnibook/Donna_Lee.xml"
+
+        # chords, melody, chord_types = chords_and_melody(omni_file)
+
+        # utils.py from Ken Deguernel, creator of Omnibook xml
+        # score_info, _, _ = xml_parser(omni_file)
+
+        # melody = []
+        # melody = file_melody(omni_file, melody) # markov chain
+
+        _, melody, _, _, _ = chords_and_21melody(omni_file)
+
+        # print(melody)
+        score = m21.stream.Score()
+
+        part = m21.stream.Part()
+        score.append(part)
+
+        eighth1 = m21.note.Note("C", quarterLength=0.5)
+        eighth2 = m21.note.Note("D", quarterLength=0.5)
+        eighth3 = m21.note.Note("E", quarterLength=0.5)
+        eighth4 = m21.note.Note("F", quarterLength=0.5)
+
+        part.append([eighth1, eighth2, eighth3, eighth4,])
+
+        score.style = m21.style
+        score.show()
+
+
+    else:
+        files_path = "Omnibook"
+        # chords_and_melody_all(files_path)
+
+        melody_all = file_melody_all(files_path)
+        print("len(melody_all)", len(melody_all))
+
+        melody_set = set(melody_all)
+        print("len(melody_set)", len(melody_set))
+
+
+
